81f52ba9eddc72f7eb98398bc478320c78a1c9ed
xzqt23 <angelo.agualada@gmail.com>
2023-02-20 14:06:38 +0800

    Update magic9.py

--- magic9.py
+++ magic9.py
@@ -11,31 +11,24 @@ random_number = random.randint(1,9)
 print(random_number)
 
 if random_number == 1:
-  answer = "Yes - definitely"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "Yes - definitely"
 elif random_number == 2:
-  answer =" It is decidedly so"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "It is decidedly so"
 elif random_number == 3:
-  answer = "Without a doubt"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "Without a doubt"
 elif random_number == 4:
-  answer = "Reply hazy, try again"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "Reply hazy, try again"
 elif random_number == 5:
-  answer = "Ask again later"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "Ask again later"
 elif random_number == 6:
-  answer = "Better not tell you now"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "Better not tell you now"
 elif random_number == 7:
-  answer = "My sources say no"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "My sources say no"
 elif random_number == 8:
-  answer = "Outlook not so good"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "Outlook not so good"
 elif random_number == 9:
-  answer = "Very doubtful"
-  print("Magic 9-Ball's answer: "+answer)
+    answer = "Very doubtful"
 else:
-  print("Error")
+    answer = "Error"
+
+print(f"Magic 9-Ball's answer: {answer}")
